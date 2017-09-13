078b2e2ffaae5868ffde621344835b1ca1fca11c
Isaiev <webisaev@gmail.com>
2017-09-13 09:46:13 +0300

    add Team model

---
+++ app/models/team.rb
@@ -0,0 +1,5 @@
+class Team < ApplicationRecord
+
+  belongs_to :owner, foreign_key: :user_id, class_name: 'User'
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170912140426) do
+ActiveRecord::Schema.define(version: 20170913064012) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "teams", force: :cascade do |t|
+    t.string   "logo"
+    t.string   "title"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_teams_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "nickname"
@@ -21,4 +30,5 @@ ActiveRecord::Schema.define(version: 20170912140426) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "teams", "users"
 end
