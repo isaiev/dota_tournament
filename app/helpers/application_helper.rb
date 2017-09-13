519973f6dcb6d8dd67bb3baf29f46a0d738968c1
Isaiev <webisaev@gmail.com>
2017-09-13 16:38:03 +0300

    add navigation class for navigation links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,11 @@
 module ApplicationHelper
 
+  ACTIVE_CLASS = 'active'.freeze
+
+  def navigation_class(path)
+    current_page?(path) ? ACTIVE_CLASS : ''
+  end
+
   def team_tournament(team_id, tournament_id)
     TeamTournament.find_by(team_id: team_id, tournament_id: tournament_id)
   end
