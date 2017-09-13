45dd51f3b8ddaddaa40cd53ae418640cf219b50e
Isaiev <webisaev@gmail.com>
2017-09-13 12:02:21 +0300

    add teams controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
 
-  root to: 'index#index'
+  root to: 'teams#index'
 
   get '/signin' => 'sessions#new', as: :signin
   get '/auth/starladder/callback', to: 'sessions#create', as: :starladder_callback
 
   resources :tournaments, only: [:show, :index]
+  resources :teams, only: [:create, :index]
 
 end
