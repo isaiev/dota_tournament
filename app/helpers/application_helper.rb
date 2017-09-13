1f9a0230c244943f25771ce2eb7e38915b5e08de
Isaiev <webisaev@gmail.com>
2017-09-13 16:09:18 +0300

    add teams list for tournament

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+
+  def team_tournament(team_id, tournament_id)
+    TeamTournament.find_by(team_id: team_id, tournament_id: tournament_id)
+  end
+
 end
--- app/helpers/tournaments_helper.rb
+++ app/helpers/tournaments_helper.rb
@@ -8,8 +8,4 @@ module TournamentsHelper
     !is_users_team_registered(tournament)
   end
 
-  def team_tournament(id)
-    @current_user.team.team_tournaments.find_by(team_id: @current_user.team.id, tournament_id: id)
-  end
-
 end
