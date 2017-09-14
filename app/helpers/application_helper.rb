3ade77c1d843061c3f9f2bf0208bf3996005e741
Isaiev <webisaev@gmail.com>
2017-09-14 11:45:21 +0300

    add navigation active link methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,8 +2,12 @@ module ApplicationHelper
 
   ACTIVE_CLASS = 'active'.freeze
 
-  def navigation_class(path)
+  def navigation_class_by_path(path)
     current_page?(path) ? ACTIVE_CLASS : ''
+  end
+
+  def navigation_class_by_controller(name)
+    controller_name == name ? ACTIVE_CLASS : ''
   end
 
   def team_tournament(team_id, tournament_id)
