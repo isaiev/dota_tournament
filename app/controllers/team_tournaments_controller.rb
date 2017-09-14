24e20a818958790c3828b6334aea16a4ad23ecfb
Isaiev <webisaev@gmail.com>
2017-09-14 12:42:26 +0300

    add restrictions TeamTournamentsController

--- app/controllers/team_tournaments_controller.rb
+++ app/controllers/team_tournaments_controller.rb
@@ -1,11 +1,21 @@
 class TeamTournamentsController < ApplicationController
 
-  def create
-    @team_tournament = TeamTournament.create!(permitted_params)
+  before_action :check_team_owner, only: [:create]
+  before_action :check_team_tournament, only: [:destroy]
 
-    respond_to do |format|
-      format.html { redirect_to tournaments_path }
-      format.js { render :replace_tournament_preview }
+  def create
+    @team_tournament = TeamTournament.new(permitted_params)
+
+    if @team_tournament.save
+      respond_to do |format|
+        format.html { redirect_to tournaments_path }
+        format.js { render :replace_tournament_preview }
+      end
+    else
+      respond_to do |format|
+        format.html { redirect_to tournaments_path }
+        format.js
+      end
     end
   end
 
@@ -19,8 +29,21 @@ class TeamTournamentsController < ApplicationController
     end
   end
 
+  private
+
   def permitted_params
     params.permit(:team_id, :tournament_id)
   end
 
+  def check_team_owner
+    @team = Team.find(permitted_params[:team_id])
+
+    forbidden unless @team.user_id == @current_user.id
+  end
+
+  def check_team_tournament
+    @team_tournament = TeamTournament.find(params[:id])
+
+    forbidden if !@team_tournament || (@team_tournament.team.owner != @current_user)
+  end
 end
